e57c032f517cc3c47beaadde9d921a0a86a748f8
joksa19 <nikolajoksimovic95@gmail.com>
2022-10-21 14:20:00 +0200

    fetch projects and make it so that js is inserting elements

--- js/index.js
+++ js/index.js
@@ -1,7 +1,49 @@
 $(window).ready(() => {
+  // preloader
   $(".preloader").addClass("preloader-hide");
 
+  // color mode
   $(".clr-btn").click((e) => {
     $("body").toggleClass("lightmode");
+  });
+
+  // fetching data and returning elements
+  $.getJSON({ url: "./data/projects.json" }).done((items) => {
+    items.forEach((item) => {
+      const image = item.img ? item.img : "../images/default_item_img.jpg";
+      $(".project-items").append(
+        `<div class="project-item">
+            <div class="project-title">
+              <h4>${item.title}</h4>
+              <div class="underline"></div>
+            </div>
+            <div class="project-img-container">
+              <img
+                src=${image}
+                alt=""
+                class="project-img"
+              />
+            </div>
+            <p class="project-info">
+              ${item.info}
+            </p>
+            <div class="project-links">
+              <a
+                href=${item.preview_link}
+                class="project-link"
+                target="_blank"
+                >visit website</a
+              >
+              <a
+                href=${item.code_link}
+                class="project-link"
+                target="_blank"
+                >&lt;code&gt;</a
+              >
+            </div>
+          </div>
+          `
+      );
+    });
   });
 });
