1437cfcdc8d07223abcb3b732dc9a53b2e1b79ff
joksa19 <nikolajoksimovic95@gmail.com>
2022-10-21 17:19:45 +0200

    add some button functionality

--- js/index.js
+++ js/index.js
@@ -1,57 +1,33 @@
-$(window).ready(() => {
+import { createItem } from "./projectItem.js";
+$(window).ready((e) => {
   // preloader
   $(".preloader").addClass("preloader-hide");
-
-  // color mode
-  $(".clr-btn").click((e) => {
-    $("body").toggleClass("lightmode");
-  });
 
   // fetching data and returning elements
   $.getJSON({ url: "./data/projects.json" }).done((items) => {
     items.forEach((item) => {
-      let toolsUsed = "";
-      // console.log(item.tools);
-      const tools = item.tools;
-      tools.forEach((tool) => {
-        toolsUsed +=
-          tool + (tools.indexOf(tool) === tools.length - 1 ? "" : ", ");
-      });
-      const image = item.img ? item.img : "../images/default_item_img.jpg";
-      $(".project-items").append(
-        `<div class="project-item">
-            <div class="project-title">
-              <h4>${item.title}</h4>
-              <div class="underline"></div>
-            </div>
-            <div class="project-img-container">
-              <img
-                src=${image}
-                alt=""
-                class="project-img"
-              />
-            </div>
-            <p class="project-info">
-              ${item.info}
-            </p>
-            <div class="project-links">
-              <a
-                href=${item.preview_link}
-                class="project-link"
-                target="_blank"
-                >visit website</a
-              >
-              <a
-                href=${item.code_link}
-                class="project-link"
-                target="_blank"
-                >&lt;code&gt;</a
-              >
-            </div>
-            <div class="project-tools"><p>{ ${toolsUsed} }</p></div>
-          </div>
-          `
-      );
+      $(".project-items").append(createItem(item));
     });
+  });
+  // color mode
+  $(".clr-btn").click(() => {
+    $("body").toggleClass("lightmode");
+  });
+  $(".projects-btn").click(() => {
+    $(window).scrollTop($(".project-section").offset().top);
+  });
+  $(".contact-btn").click(() => {
+    $(window).scrollTop($(".contact-section").offset().top);
+  });
+  $(".up-btn").click(() => {
+    $(window).scrollTop(0);
+  });
+  $(window).scroll(() => {
+    console.log($(window).scrollTop(), $(window).height() * 1.2);
+    if ($(window).scrollTop() >= $(window).height() * 1.2) {
+      $(".up-btn").removeClass("up-btn-show");
+    } else {
+      $(".up-btn").addClass("up-btn-show");
+    }
   });
 });
---
+++ js/projectItem.js
@@ -0,0 +1,41 @@
+export const createItem = (item) => {
+  let toolsUsed = "";
+  // console.log(item.tools);
+  const tools = item.tools;
+  tools.forEach((tool) => {
+    toolsUsed += tool + (tools.indexOf(tool) === tools.length - 1 ? "" : ", ");
+  });
+  const image = item.img ? item.img : "../images/default_item_img.jpg";
+  return `<div class="project-item">
+            <div class="project-title">
+              <h4>${item.title}</h4>
+              <div class="underline"></div>
+            </div>
+            <div class="project-img-container">
+              <img
+                src=${image}
+                alt=""
+                class="project-img"
+              />
+            </div>
+            <p class="project-info">
+              ${item.info}
+            </p>
+            <div class="project-links">
+              <a
+                href=${item.preview_link}
+                class="project-link"
+                target="_blank"
+                >visit website</a
+              >
+              <a
+                href=${item.code_link}
+                class="project-link"
+                target="_blank"
+                >&lt;code&gt;</a
+              >
+            </div>
+            <div class="project-tools"><p>{ ${toolsUsed} }</p></div>
+          </div>
+          `;
+};
