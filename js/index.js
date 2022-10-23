54f0823ecc6516ee4582cb638dc651699dbb4a2c
joksa19 <nikolajoksimovic95@gmail.com>
2022-10-23 23:54:02 +0200

    add age counter

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 import { createItem } from "./projectItem.js";
 $(window).ready((e) => {
   // preloader
-  $(".preloader").addClass("preloader-hide");
+  setTimeout(() => {
+    $(".preloader").addClass("preloader-hide");
+  }, 1000);
 
   // fetching data and returning elements
   $.getJSON({ url: "./data/projects.json" }).done((items) => {
@@ -23,14 +25,27 @@ $(window).ready((e) => {
     );
     const projectItems = $(".project-item").toArray();
     projectItems.forEach((item) => {
-      console.log(item);
       observer.observe(item);
     });
   });
 
   // color mode
+  const local_storage = $(window)[0].localStorage;
+  let mode = local_storage.getItem("mode");
+
+  if (mode === "lightmode") {
+    $("body").addClass("lightmode");
+  }
+
   $(".clr-btn").click(() => {
-    $("body").toggleClass("lightmode");
+    mode = local_storage.getItem("mode");
+    if (mode === "lightmode") {
+      local_storage.setItem("mode", "darkmode");
+      $("body").removeClass("lightmode");
+    } else {
+      local_storage.setItem("mode", "lightmode");
+      $("body").addClass("lightmode");
+    }
   });
 
   // btn functionalities
@@ -50,4 +65,31 @@ $(window).ready((e) => {
       $(".up-btn").addClass("up-btn-hide");
     }
   });
+
+  // age-calc
+  const my_dob = new Date("12/18/1995");
+  const min = 1000 * 60;
+  const hour = 60 * min;
+  const day = 24 * hour;
+  const year = 365 * day;
+
+  setInterval(() => {
+    const curr_age = new Date().getTime() - my_dob;
+    const years = Math.floor(curr_age / year);
+    const days = Math.floor((curr_age - years * year) / day);
+    const hours = Math.floor((curr_age - years * year - days * day) / hour);
+    const minutes = Math.floor(
+      (curr_age - years * year - days * day - hours * hour) / min
+    );
+    const seconds = Math.floor(
+      (curr_age - years * year - days * day - hours * hour - minutes * min) /
+        1000
+    );
+
+    $("#age-years").text(`${years}`);
+    $("#age-days").text(`${days < 10 ? `0${days}` : days}`);
+    $("#age-hours").text(`${hours < 10 ? `0${hours}` : hours}`);
+    $("#age-minutes").text(` ${minutes < 10 ? `0${minutes}` : minutes}`);
+    $("#age-seconds").text(`${seconds < 10 ? `0${seconds}` : seconds}`);
+  }, 1000);
 });
